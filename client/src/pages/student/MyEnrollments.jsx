760dbe89939addd412203ae817722cb64b305d91
UMANG-2003 <umangkumar20003@gmail.com>
2025-08-03 00:03:43 +0530

    feat: Enhance MyEnrollments page with progress tracking, integrate progress bars, and add Footer component

--- client/src/pages/student/MyEnrollments.jsx
+++ client/src/pages/student/MyEnrollments.jsx
@@ -1,15 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AppContext } from "../../context/AppContext";
+import {Line} from "rc-progress";
+import Footer from '../../components/student/Footer'
 
 function MyEnrollments() {
-  const { enrolledCourses, calculateCourseDuration } = useContext(AppContext);
+  const { enrolledCourses, calculateCourseDuration,navigate} = useContext(AppContext);
+  const [progressArray, setProgressArray] = useState([
+    { lecturesCompleted: 2, totalLectures: 4 },
+    { lecturesCompleted: 4, totalLectures: 5 },
+    { lecturesCompleted: 1, totalLectures: 6 },
+    { lecturesCompleted: 4, totalLectures: 4 },
+    { lecturesCompleted: 2, totalLectures: 3 },
+    { lecturesCompleted: 5, totalLectures: 6 },
+    { lecturesCompleted: 10, totalLectures: 10 },
+    { lecturesCompleted: 3, totalLectures: 7 },
+  ]);
 
-  return (
-    <div className="bg-black min-h-screen text-white px-4 md:px-36 pt-10">
+  return <>
+    <div className="bg-black min-h-screen text-white px-4 md:px-36 py-20">
       <h2 className="text-2xl font-semibold mb-6">My Enrollments</h2>
 
       <div className="overflow-x-auto">
-        <table className="w-full border-collapse min-w-[600px]">
+        <table className="w-full border border-gray-900 min-w-[600px]">
           <thead className="bg-gray-800 text-gray-300 text-sm uppercase">
             <tr>
               <th className="px-4 py-3 font-semibold text-left">Course</th>
@@ -28,18 +40,26 @@ function MyEnrollments() {
                     alt="thumbnail"
                   />
                   <div>
-                    <p className="text-base font-medium">{course.courseTitle}</p>
+                    <p className="text-base font-medium">
+                      {course.courseTitle}
+                    </p>
+                    <Line strokeColor={"red"} strokeWidth={2} percent={(progressArray[index].lecturesCompleted/progressArray[index].totalLectures)*100} className="bg-gray-100 rounded-full" />
                   </div>
                 </td>
                 <td className="px-4 py-4 text-sm">
                   {calculateCourseDuration(course)}
                 </td>
                 <td className="px-4 py-4 text-sm">
-                  5/10 <span className="text-gray-400">Lectures</span>
+                  {progressArray[index] && (
+                    <>
+                      {`${progressArray[index].lecturesCompleted}/${progressArray[index].totalLectures}`}
+                      <span className="text-gray-400"> - Lectures</span>
+                    </>
+                  )}
                 </td>
                 <td className="px-4 py-4">
-                  <button className="bg-blue-600 hover:bg-blue-700 text-white px-3 py-1 rounded text-sm">
-                    Ongoing
+                  <button className={`${progressArray[index].lecturesCompleted !== progressArray[index].totalLectures ? "bg-blue-600 hover:bg-blue-700" : "bg-green-600 hover:bg-green-700"} text-white px-3 py-1 rounded text-sm w-[100px]`} onClick={()=>navigate("/player/"+course._id)}>
+                    {(progressArray[index].lecturesCompleted===progressArray[index].totalLectures)? "Completed":"Ongoing"}
                   </button>
                 </td>
               </tr>
@@ -48,7 +68,10 @@ function MyEnrollments() {
         </table>
       </div>
     </div>
-  );
+    <Footer />
+  </>
+
+  
 }
 
 export default MyEnrollments;
