6126e6e4a84a8f42995d4327eefaf543a8682f5a
UMANG-2003 <umangkumar20003@gmail.com>
2025-08-16 11:00:49 +0530

    feat: Implement course management features with educator role updates and webhook handling

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,6 +2,7 @@ import { createContext, use, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 export const AppContext = createContext();
 import { useNavigate } from "react-router-dom";
+import {useAuth,useUser} from "@clerk/clerk-react"
 
 export const AppContextProvider = (props) => {
   const currency = import.meta.env.VITE_CURRENCY || "$";
@@ -9,6 +10,9 @@ export const AppContextProvider = (props) => {
   const [isEducator, setIsEducator] = useState(true);
   const [enrolledCourses,setEnrolledCourses]=useState([]);
   const navigate = useNavigate();
+
+  const {getToken}=useAuth()
+  const {user}=useUser()
 
   const fetchCourses = () => {
     setallCourses(dummyCourses);
@@ -76,6 +80,15 @@ export const AppContextProvider = (props) => {
     fetchCourses();
     fetchUserEnrolledCourses();
   }, []);
+
+  const logToken = async ()=>{
+    console.log(await getToken());
+  }
+  useEffect(()=>{
+      if(user){
+       logToken();
+      }
+  },[user])
 
 
   const value = {
--- server/server.js
+++ server/server.js
@@ -2,17 +2,30 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
-import { clerkWebhooks } from './controllers/webhooks.js'
+import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js'
+import educatorRouter from './routes/educatorRoutes.js'
+import { clerkMiddleware } from '@clerk/express'
+import connectCloudinary from './configs/cloudinary.js'
+import userRouter from './routes/userRoutes.js'
 
 const app = express()
+await connectDB();
+await connectCloudinary();
 
 app.use(cors())
-await connectDB();
+app.use(clerkMiddleware())
+
 
 app.get('/',(req,res)=>{
    res.send("API is  Working")
 })
 app.post('/clerk',express.json(),clerkWebhooks)
+app.use('/api/educator',express.json(),educatorRouter)
+app.use('/api/course',express.json)
+app.use('/api/user',express.json(),userRouter)
+app.post('/stripe',express.raw({type:'application/json'}),stripeWebhooks)
+
+
 
 const PORT = process.env.PORT || 5000;
 
